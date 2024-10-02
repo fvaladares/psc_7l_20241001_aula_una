2ac75f55dcab8766acb2c6fc9240170a03510a0c
Fabricio Valadares <fabricio.valadares@gmail.com>
2024-10-01 22:01:44 -0300

    Final version

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,24 @@ public class Main {
         Aluno aluno;
         String matricula;
         double nota;
-        int qtdFichas = 100;
+        int qtdFichas = 4;
         Ficha ficha = new Ficha();
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < qtdFichas; i++) {
             System.out.println("Informe a matricula do "
-                    + i + 1 + "° aluno.");
+                    + (i + 1) + "° aluno.");
             matricula = scanner.nextLine();
             System.out.println("Informe a nota do "
-                    + i + 1 + "° aluno.");
+                    + (i + 1) + "° aluno.");
             nota = scanner.nextInt();
             aluno = new Aluno(matricula, nota);
             ficha.comparadorDeNotas(aluno);
 //            ficha.comparadorDeNotas(new Aluno(matricula, nota));
-
-
         }
+
+        System.out.println();
+        System.out.println(ficha.mostrarNotas());
 
     }
 }
